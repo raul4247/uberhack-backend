ebde27043b63ce0349f6c3de5d6d284456e5601a
Raul Mansur <raulfmansur@hotmail.com>
2019-07-07 04:23:17 -0300

    Rotas para get de classes de kids e drivers criadas.

--- src/creating.sql
+++ src/creating.sql
@@ -1,4 +1,5 @@
 DROP TABLE kids;
+DROP TABLE drivers;
 
 
 CREATE TABLE `kids`
@@ -20,10 +21,15 @@ INSERT INTO kids (name, age) values ('Maria', 6);
 CREATE TABLE `drivers`
 (
 			`name` text NOT NULL,
-			`age` int NOT NULL,
+			`phone` text NOT NULL,
 			`id` bigint(20) NOT NULL AUTO_INCREMENT,
 			PRIMARY KEY (`id`)
 );
 
 -- criando informações dummy para motorista
 
+INSERT INTO drivers (name, phone) values ('Eusebio', "(31) 91235-1654");
+INSERT INTO drivers (name, phone) values ('Dalton', "(31) 91234-1236");
+INSERT INTO drivers (name, phone) values ('Wanderson', "(31) 95647-6543");
+INSERT INTO drivers (name, phone) values ('Jailson', "(31) 91735-9132");
+INSERT INTO drivers (name, phone) values ('Rony', "(31) 93013-1842");
--- src/routes.php
+++ src/routes.php
@@ -25,3 +25,18 @@ $app->get('/getAllKids',function ($request, $response, $args){
 	$conn->close();
 	return  json_encode($kids);
 });
+
+$app->get('/getAllDrivers',function ($request, $response, $args){	
+	$conn = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+	$sql = "SELECT * FROM drivers";
+	$result = $conn->query($sql);
+
+	$drivers = array();
+
+	while($row = $result->fetch_assoc()){
+	  $drivers[] = array_map('utf8_encode', $row);
+	}
+	
+	$conn->close();
+	return  json_encode($drivers);
+});
